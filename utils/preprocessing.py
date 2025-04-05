d7fbbe079329dcdfd04bb926966af821a4da5f1e
sameekshaa33 <sameekshaa.33@gmail.com>
2025-04-05 22:43:10 +0530

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,13 +1,11 @@
 # utils/preprocessing.py
 import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 
 def preprocess_text(text):
     text = str(text).lower()
     text = re.sub(r'[^a-zA-Z\s]', '', text)
-    tokens = nltk.word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-    return ' '.join([lemmatizer.lemmatize(w) for w in tokens 
-                    if w not in stopwords.words('english')])
+    tokens = text.split()  # Replaces nltk.word_tokenize
+    stopwords = set([
+        'the', 'and', 'is', 'in', 'to', 'of', 'a', 'an', 'this', 'that', 'for', 'on', 'with', 'as', 'it', 'by', 'at', 'from'
+    ])
+    return ' '.join([word for word in tokens if word not in stopwords])
